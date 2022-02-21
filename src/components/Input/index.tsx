bc9dbd9f53b972e13360d1a3a79cd9daf266a070
Vinicius Pinheiro <vinicius.pinheiro@dxc.com>
2022-02-21 20:21:27 -0300

    refactor: transform all components to TS

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,13 +3,25 @@ import {
   useRef,
   useState,
   useCallback,
-} from 'react';
+  InputHTMLAttributes,
+} from "react";
 
-import { useField } from '@unform/core';
+import { useField } from "@unform/core";
 
-import { Container } from './styles';
+// styles:
+import { Container } from "./styles";
 
-const Input = ({ name, icon: Icon, ...rest }) => {
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode;
+  name: string;
+}
+
+const Input: React.FC<InputProps> = ({
+  name,
+  icon: Icon,
+  placeholder,
+  ...rest
+}: InputProps) => {
   const inputRef = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
@@ -24,26 +36,29 @@ const Input = ({ name, icon: Icon, ...rest }) => {
   const handleInputBlur = useCallback(() => {
     setIsFocused(false);
 
-    setIsFilled(!!inputRef.current?.value);
+    const typesInputRef: any = inputRef.current;
+
+    setIsFilled(!!typesInputRef?.value);
   }, []);
 
   useEffect(() => {
     registerField({
       name: fieldName,
       ref: inputRef.current,
-      path: 'value',
+      path: "value",
     });
   }, [fieldName, registerField]);
 
   return (
     <Container isFilled={isFilled} isFocused={isFocused}>
-      {Icon && <Icon size={20} />}
+      {Icon && Icon}
 
       <input
         onFocus={handleInputFocus}
         onBlur={handleInputBlur}
         defaultValue={defaultValue}
         ref={inputRef}
+        placeholder={placeholder}
         {...rest}
       />
     </Container>
